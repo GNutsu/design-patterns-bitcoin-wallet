d4e49d8d6b30c1dcf0cd98cd912f63ef064c9671
styxBeneath <xucishvilisaba@gmail.com>
2024-02-04 22:34:05 +0400

    db module

--- bitcoinwallet/core/repository/repository_factory.py
+++ bitcoinwallet/core/repository/repository_factory.py
@@ -1,14 +1,30 @@
-from abc import ABC, abstractmethod
+from typing import Dict, Type
 
-from bitcoinwallet.core.repository.repository import IRepository, NullRepository
-
-
-class IRepositoryFactory(ABC):
-    @abstractmethod
-    def get_repository(self) -> IRepository:
-        pass
+from bitcoinwallet.core.model.entity import Entity
+from bitcoinwallet.core.repository.repository import IRepository, Repository
+from definitions import DB_NAME
 
 
-class NullRepositoryFactory(IRepositoryFactory):
-    def get_repository(self) -> IRepository:
-        return NullRepository()
+class RepositoryFactory:
+    _instance = None
+
+    def __init__(self) -> None:
+        self._dao_map: Dict[Type[Entity], IRepository] = {}
+
+    @staticmethod
+    def get_db_path() -> str:
+        return DB_NAME
+
+    @classmethod
+    def get_instance(cls) -> "RepositoryFactory":
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+    def get_repository(self, entity_class: Type[Entity]) -> IRepository:
+        if entity_class not in self._dao_map:
+            self._initialize_repository(entity_class)
+        return self._dao_map[entity_class]
+
+    def _initialize_repository(self, entity_class: Type[Entity]) -> None:
+        self._dao_map[entity_class] = Repository(entity_class, self.get_db_path())
--- definitions.py
+++ definitions.py
@@ -1,1 +1,2 @@
 BITCOIN_FEE_PERCENTAGE = 1.5
+DB_NAME = "bw_db.db"
