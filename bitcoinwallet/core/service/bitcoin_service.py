5212ffd4ca80bd7e9d690986bc41f7db7ef3ab4e
niaChkhaidze <niachkhaidze94@gmail.com>
2024-02-05 21:56:17 +0400

    currency calculations

--- bitcoinwallet/core/service/bitcoin_service.py
+++ bitcoinwallet/core/service/bitcoin_service.py
@@ -15,6 +15,7 @@ from bitcoinwallet.core.service.transaction_service import (
 )
 from bitcoinwallet.core.service.user_service import IUserService, NullUserService
 from bitcoinwallet.core.service.wallet_service import IWalletService, NullWalletService
+from bitcoinwallet.core.util import CurrencyExchangeUtil
 from definitions import BITCOIN_FEE_PERCENTAGE
 
 TBitcoinService = TypeVar("TBitcoinService", bound="BitcoinServiceBuilder")
@@ -83,19 +84,26 @@ class BitcoinService(IBitcoinService):
         second_owner = self.wallet_service.get_owner_api_key(address=to_wallet_addr)
         fee_for_transaction = 0
 
+        amount_in_satoshi = CurrencyExchangeUtil.bitcoin_to_satoshi(amount)
+
         if first_owner != second_owner:
-            fee_for_transaction = math.ceil(amount * BITCOIN_FEE_PERCENTAGE / 100)
+            fee_for_transaction = math.ceil(
+                amount_in_satoshi * BITCOIN_FEE_PERCENTAGE / 100
+            )
+
         self.logger.info(f"Fee for transaction is:  {fee_for_transaction}")
         self.wallet_service.withdraw(
             user_api_key=user_api_key,
             wallet_address=from_wallet_addr,
-            amount=(amount + fee_for_transaction),
+            amount=amount_in_satoshi + fee_for_transaction,
         )
-        self.wallet_service.deposit(wallet_address=to_wallet_addr, amount=amount)
+        self.wallet_service.deposit(
+            wallet_address=to_wallet_addr, amount=amount_in_satoshi
+        )
         return self.transaction_service.create_transaction(
             from_addr=from_wallet_addr,
             to_addr=to_wallet_addr,
-            amount=amount,
+            amount=amount_in_satoshi,
             fee_cost=fee_for_transaction,
         )
 
@@ -111,10 +119,14 @@ class BitcoinService(IBitcoinService):
         self, api_key: str, wallet_address: str
     ) -> Tuple[float, float]:
         self.logger.info(f"Fetching balance for wallet: {wallet_address}")
-        btc_balance = self.wallet_service.get_wallet_balance(api_key, wallet_address)
+        satoshi_balance = self.wallet_service.get_wallet_balance(
+            api_key, wallet_address
+        )
+        btc_balance = CurrencyExchangeUtil.satoshi_to_bitcoin(satoshi_balance)
 
-        btc_to_usd_rate = self.currency_api_client.get_btc_to_usd_rate()
-        usd_balance = btc_balance * btc_to_usd_rate
+        usd_balance = CurrencyExchangeUtil.bitcoin_to_usd(
+            btc_balance, self.currency_api_client
+        )
         return btc_balance, usd_balance
 
     def create_wallet(self, api_key: str) -> Tuple[str, float, float]:
--- bitcoinwallet/core/util.py
+++ bitcoinwallet/core/util.py
@@ -1,7 +1,24 @@
 from datetime import datetime
 
-from definitions import FORMAT
+from bitcoinwallet.core.service.currency_api_client import ICurrencyApiClient
+from definitions import FORMAT, SATOSHIS_PER_BITCOIN
 
 
 def datetime_now() -> str:
     return datetime.now().strftime(FORMAT)
+
+
+class CurrencyExchangeUtil:
+    @staticmethod
+    def bitcoin_to_satoshi(amount_in_btc: float) -> int:
+        return int(amount_in_btc * SATOSHIS_PER_BITCOIN)
+
+    @staticmethod
+    def satoshi_to_bitcoin(amount_in_satoshi: int) -> float:
+        return amount_in_satoshi / SATOSHIS_PER_BITCOIN
+
+    @staticmethod
+    def bitcoin_to_usd(
+        amount_in_btc: float, btc_usd_currency_src_client: ICurrencyApiClient
+    ) -> float:
+        return btc_usd_currency_src_client.get_btc_to_usd_rate() * amount_in_btc
